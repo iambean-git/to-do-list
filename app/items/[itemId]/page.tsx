6119b527d7728f53fc335e077b721f99f41e1bf7
iambean-git <peb1021@naver.com>
2025-03-27 19:27:11 +0900

    fix : build 오류 수정

--- app/items/[itemId]/page.tsx
+++ app/items/[itemId]/page.tsx
@@ -1,13 +1,9 @@
 import { fethcTodoItem } from "@/app/lib/actions";
 import ChecklistDetailPage from "@/app/ui/items/ChecklistDetailPage";
 
-interface PageProps {
-    params: {
-      itemId: string;
-    };
-  }
+type PageParams = Promise<{ itemId: string }>;
 
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: {params : PageParams}) {
     const { itemId } = await params;
     const data = await fethcTodoItem(itemId);
     // console.log("data :", data);
